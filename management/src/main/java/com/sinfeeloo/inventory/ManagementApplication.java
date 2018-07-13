720bb5de2bea79cf253f3942d3c4e7dc72d3b473
mahuaju <mhjdxlwb@gmail.com>
2018-07-13 10:12:57 +0800

    项目部署到阿里云centos服务器的配置

--- management/src/main/java/com/sinfeeloo/inventory/ManagementApplication.java
+++ management/src/main/java/com/sinfeeloo/inventory/ManagementApplication.java
@@ -2,10 +2,18 @@ package com.sinfeeloo.inventory;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableAutoConfiguration
 @SpringBootApplication
 @MapperScan("com.sinfeeloo.inventory.mapper")
+@EnableWebMvc
+@EnableCaching
+@EnableScheduling
 public class ManagementApplication {
 
 	public static void main(String[] args) {
